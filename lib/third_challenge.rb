37ebba8006a9e74026400a482b39f7055bd50cba
J.D. Bean <jonathandbean@gmail.com>
2018-02-16 19:44:29 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,20 +15,24 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: => "Lord Capulet",
+        age: => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
